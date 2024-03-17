1354c23e343f1a7527a1f57a51f58fb440ea4065
Victor Jesus <161655169+toriodev@users.noreply.github.com>
2024-03-17 08:45:31 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+
+/* Desafio surpresa | Programar um algoritmo de centro pokemon
+
+O QUE PRECISA SER FEITO ?
+Crie um código para armazenar o máximo de informações possível dos pokemon da imagem ao lado e ao final, para cada um, exiba uma mensagem de saída escrita o nome do pokemon concatenado com "Cadastrado com sucesso"
+*/
+
+let nomePokemon = ["Poochyena", "Zigzagoon", "Dragonite", "Hurmple"]
+let levelPokemon = ["Lv2", "Lv3", "Lv5"]
+let sexoPokemon = ["M", "F"]
+
+// print primeiro pokemon
+console.log("Cadastro realizado com sucesso!\n Nome:" + nomePokemon[0] + "\n Level: " + levelPokemon[0] + "\n Sexo: " + sexoPokemon[0])
+console.log("Cadastro realizado com sucesso!\n Nome:" + nomePokemon[1] + "\n Level: " + levelPokemon[0] + "\n Sexo: " + sexoPokemon[1])
+console.log("Cadastro realizado com sucesso!\n Nome:" + nomePokemon[2] + "\n Level: " + levelPokemon[2] + "\n Sexo: " + sexoPokemon[0])
+console.log("Cadastro realizado com sucesso!\n Nome:" + nomePokemon[2] + "\n Level: " + levelPokemon[2] + "\n Sexo: " + sexoPokemon[1])
+console.log("Cadastro realizado com sucesso!\n Nome:" + nomePokemon[2] + "\n Level: " + levelPokemon[2] + "\n Sexo: " + sexoPokemon[1])
+console.log("Cadastro realizado com sucesso!\n Nome:" + nomePokemon[0] + "\n Level: " + levelPokemon[1] + "\n Sexo: " + sexoPokemon[1])
+console.log("Cadastro realizado com sucesso!\n Nome:" + nomePokemon[3] + "\n Level: " + levelPokemon[0] + "\n Sexo: " + sexoPokemon[0])
